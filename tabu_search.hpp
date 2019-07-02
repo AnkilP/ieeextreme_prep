aea6ddba7f26acbb5bef9ee053b5124e36f0b80d
AnkilP <a293pate@uwaterloo.ca>
2019-07-02 15:24:38 -0400

    csv reading

--- tabu_search.hpp
+++ tabu_search.hpp
@@ -1,2 +1,48 @@
 #include <iostream>
-#include 
+#include <algorithm>
+#include <vector>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+class CSVReader
+{
+	std::string fileName;
+ 
+    public:
+        CSVReader(std::string filename) :
+                fileName(filename)
+        { }
+        void get(std::vector<std::vector<int>> &);
+};
+
+void CSVReader::get(std::vector<std::vector<int>> & dataList)
+{
+	std::ifstream file(fileName);
+ 
+	std::string line;
+
+	while(std::getline(file,line))
+    {
+        std::stringstream lineStream(line);
+        std::string cell;
+        std::vector<int> parsedRow;
+        while(std::getline(lineStream,cell,','))
+        {
+            parsedRow.push_back(std::stoi(cell));
+        }
+
+        dataList.push_back(parsedRow);
+    }
+	// Close the File
+	file.close();
+}
+
+
+class tabu_search{
+
+    public:
+        tabu_search(std::string fileName, );
+
+
+};
